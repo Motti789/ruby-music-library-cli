a8793114e13810eba1f50a54dfa92849cfb33af5
Motti789 <mchillywilly@gmail.com>
2021-08-25 14:05:25 -0400

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,28 @@
+class Genre
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+    end    
+
+    def self.all
+     @@all
+    end 
+
+    def save
+     self.class.all << self
+     self
+    end 
+
+    def self.destroy_all
+     all.clear
+    end
+
+    def self.create(name)
+      new(name).save
+    end
+
+end
